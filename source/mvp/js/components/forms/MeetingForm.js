5cf97c128d8e968d52e8ad340df1373f4bf1367d
aperelson1 <aperelson@icloud.com>
2026-05-31 13:54:25 -0700

    JSdocs added to files in /components

--- source/mvp/js/components/forms/MeetingForm.js
+++ source/mvp/js/components/forms/MeetingForm.js
@@ -2,12 +2,12 @@
  * Schedule team meeting form (markup from templates/partials.html).
  * @module components/forms/MeetingForm
  */
-
 import { renderTemplate } from '../../utils/templateEngine.js';
-
 /**
- * @param {string} [defaultDate]
- * @param {string} [defaultTime]
+ * Renders the schedule-team-meeting form by expanding its HTML template,
+ * pre-filling the date and time fields with the supplied defaults.
+ * @param {string} [defaultDate] - Initial value for the meeting date field; defaults to an empty string.
+ * @param {string} [defaultTime] - Initial value for the meeting time field; defaults to `'10:00 AM'`.
  * @returns {string} HTML form markup
  */
 export function MeetingForm(defaultDate = '', defaultTime = '10:00 AM') {
